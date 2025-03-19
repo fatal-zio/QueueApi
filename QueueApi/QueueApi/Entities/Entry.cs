78426e094a0a0ab689200993e0e023d8c0cd8b34
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2025-03-18 19:49:49 -0500

    Created Entities and empty controllers.

---
+++ QueueApi/QueueApi/Entities/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QueueApi.Entities
+{
+    public class Entry
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public int SessionId { get; set; }
+        public Session Session { get; set; }
+    }
+}
---
+++ QueueApi/QueueApi/Entities/Session.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueueApi.Entities
+{
+    public class Session
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+        public ICollection<Entry> Entries { get; set; } = [];
+    }
+}
