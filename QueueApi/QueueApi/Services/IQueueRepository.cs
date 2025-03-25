9b1e108799a7c9604dc3b76e523f00d80ffb1b8d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2025-03-25 18:47:16 -0500

    Completed initial Repository code.

--- QueueApi/QueueApi/Services/IQueueRepository.cs
+++ QueueApi/QueueApi/Services/IQueueRepository.cs
@@ -1,7 +1,21 @@
-﻿namespace QueueApi.Services
+﻿using QueueApi.Entities;
+
+namespace QueueApi.Services
 {
     public interface IQueueRepository
     {
-
+        Task<IEnumerable<Session>> GetSessionsAsync();
+        Task<Session?> GetSessionAsync(int sessionId, bool includeEntries);
+        Task<IEnumerable<Entry>> GetEntriesAsync(int sessionId);
+        Task<Entry?> GetEntryAsync(int sessionId, int entryId);
+        Task<Status?> GetStatusAsync(int statusId);
+        Task<bool> SessionExistsAsync(int sessionId);
+        Task<bool> EntryExistsAsync(int sessionId, int entryId);
+        Task<bool> StatusExistsAsync(int statusId);
+        Task<bool> SaveAsync();
+        Task AddSessionAsync(Session session);
+        Task AddEntryAsync(int sessionId, Entry entry);
+        void DeleteSessionAsync(Session session);
+        void DeleteEntryAsync(Entry entry);
     }
 }
--- QueueApi/QueueApi/Services/QueueRepository.cs
+++ QueueApi/QueueApi/Services/QueueRepository.cs
@@ -1,7 +1,80 @@
-﻿namespace QueueApi.Services
-{
-    public class QueueRepository
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using QueueApi.DbContexts;
+using QueueApi.Entities;
 
+namespace QueueApi.Services
+{
+    public class QueueRepository(QueueContext context) : IQueueRepository
+    {
+        private readonly QueueContext _context =
+            context ?? throw new ArgumentNullException(nameof(context));
+
+        public async Task AddEntryAsync(int sessionId, Entry entry)
+        {
+            var session = await GetSessionAsync(sessionId, false);
+
+            session?.Entries.Add(entry);
+        }
+
+        public async Task AddSessionAsync(Session session)
+        {
+            await _context.Sessions.AddAsync(session);
+        }
+
+        public void DeleteEntryAsync(Entry entry)
+        {
+            _context.Entries.Remove(entry);
+        }
+
+        public void DeleteSessionAsync(Session session)
+        {
+            _context.Sessions.Remove(session);
+        }
+
+        public Task<bool> EntryExistsAsync(int sessionId, int entryId)
+        {
+            return _context.Entries.AnyAsync(e => e.Id == entryId && e.SessionId == sessionId);
+        }
+
+        public async Task<IEnumerable<Entry>> GetEntriesAsync(int sessionId)
+        {
+            return await _context.Entries.Where(e => e.SessionId == sessionId).ToListAsync();
+        }
+
+        public async Task<Entry?> GetEntryAsync(int sessionId, int entryId)
+        {
+            return await _context.Entries.FirstOrDefaultAsync(e => e.Id == entryId && e.SessionId == sessionId);
+        }
+
+        public Task<Session?> GetSessionAsync(int sessionId, bool includeEntries)
+        {
+            return includeEntries ? _context.Sessions.Include(s => s.Entries).FirstOrDefaultAsync(s => s.Id == sessionId) :
+               _context.Sessions.FirstOrDefaultAsync(s => s.Id == sessionId);
+        }
+
+        public async Task<IEnumerable<Session>> GetSessionsAsync()
+        {
+            return await _context.Sessions.ToListAsync();
+        }
+
+        public async Task<Status?> GetStatusAsync(int statusId)
+        {
+            return await _context.Statuses.FirstOrDefaultAsync(s => s.Id == statusId);
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0;
+        }
+
+        public async Task<bool> SessionExistsAsync(int sessionId)
+        {
+            return await _context.Sessions.AnyAsync(s => s.Id == sessionId);
+        }
+
+        public async Task<bool> StatusExistsAsync(int statusId)
+        {
+            return await _context.Statuses.AnyAsync(s => s.Id == statusId);
+        }
     }
 }
