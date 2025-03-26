9849041b956bfcf4eff1c7bfad8d16e13bc67837
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2025-03-25 19:13:37 -0500

    Added Serilog.

--- QueueApi/QueueApi/Program.cs
+++ QueueApi/QueueApi/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using QueueApi.DbContexts;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File("logs/queue.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
